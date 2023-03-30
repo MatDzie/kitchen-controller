4a2aa3e67a724b1666f5d198301bdced85245123
Mateusz Dziedzic <mateuszdziedzic90@gmail.com>
2023-03-30 19:03:00 +0200

    Initial commit

---
+++ src/main/java/com/mat/kitchencontroller/KitchenControllerApplication.java
@@ -0,0 +1,13 @@
+package com.mat.kitchencontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchenControllerApplication.class, args);
+	}
+
+}
