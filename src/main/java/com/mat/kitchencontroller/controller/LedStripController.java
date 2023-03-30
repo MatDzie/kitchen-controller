d1d0006d868e0600e3c2f1af13d3dc21c0b46432
Mateusz Dziedzic <mateuszdziedzic90@gmail.com>
2023-03-30 23:26:29 +0200

    Correct naming and remove unused variable.

--- src/main/java/com/mat/kitchencontroller/controller/LedStripController.java
+++ src/main/java/com/mat/kitchencontroller/controller/LedStripController.java
@@ -1,7 +1,6 @@
 package com.mat.kitchencontroller.controller;
 
 import com.mat.kitchencontroller.component.LedStrip;
-import com.pi4j.context.Context;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,19 +10,19 @@ public class LedStripController {
 
     private final LedStrip ledStrip;
 
-    public LedStripController(Context pi4j, LedStrip ledStrip) {
+    public LedStripController(LedStrip ledStrip) {
         this.ledStrip = ledStrip;
     }
 
     @GetMapping("/on/{brightness}")
     public String on(@PathVariable("brightness") Integer brightness) {
         ledStrip.turnOn(brightness);
-        return "Turning LED on with brightness of: " + brightness;
+        return "Turning LED strip ON with brightness of: " + brightness;
     }
 
     @GetMapping("/off")
     public String off() {
         ledStrip.turnOff();
-        return "Turning LED off!";
+        return "Turning LED strip OFF!";
     }
 }
