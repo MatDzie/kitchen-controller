73dbf36ba328730eeb39fee74c17c5514ed9b2c5
Mateusz Dziedzic <mateuszdziedzic90@gmail.com>
2023-04-15 00:53:10 +0200

    Add validation with GPT-3.5

--- src/main/java/com/mat/kitchencontroller/controllers/LightSourceController.java
+++ src/main/java/com/mat/kitchencontroller/controllers/LightSourceController.java
@@ -1,11 +1,15 @@
 package com.mat.kitchencontroller.controllers;
 
 import com.mat.kitchencontroller.services.LightSourceService;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Validated
 public class LightSourceController {
 
     private final LightSourceService lightSourceService;
@@ -14,10 +18,8 @@ public class LightSourceController {
         this.lightSourceService = lightSourceService;
     }
 
-    // TODO: Add validation 0-100
-    // TODO: Change API to be compatible with Google Home
     @GetMapping("/light/on/{brightness}")
-    public String on(@PathVariable("brightness") Integer brightness) {
+    public String on(@PathVariable("brightness") @Min(10) @Max(100) Integer brightness) {
         lightSourceService.turnOn(brightness);
         return "Turning light ON with brightness of: " + brightness;
     }
